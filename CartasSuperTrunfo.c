3076675d47cb29a0aa7de86a2c2d95b5a95c2c1e
Wagnerffs <wagnerffs@hotmail.com>
2024-11-11 14:43:06 +0000

    inicio aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,14 +6,16 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main(){
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    // Sugestão: Defina variáveis separadas para cada atributo da cidade;
       
       
-    //criação das variáveis
+    //criação das variáveis;
     int cod = 25;
-    int pop = 360;
-    int area = 360;
-    float pib = 36.5;
+    int pop = 360.33;
+    float area = 360.123;
+    float pib = 365.321;
+    double densidade = 366.852; //população dividido pela area;
+    float pibpc = 365.321
     int turismo = 25;
     char nome[50] = "parana";
 
@@ -25,7 +27,7 @@ int main(){
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
 
 
-    //criação das entradas e saídas 
+    //criação das entradas e saídas; 
     printf("Digite cod. da cidade\n");
     scanf("%d", &cod);
 
@@ -36,7 +38,7 @@ int main(){
     scanf("%d", &pop);
 
     printf("Qual é a área\n");
-    scanf("%d", &area);
+    scanf("%f", &area);
 
     printf("Qual o PIB\n");
     scanf("%f", &pib);
@@ -44,8 +46,11 @@ int main(){
     printf("Quantos pontos turísticos\n");
     scanf("%d", &turismo);
 
+    densidade = pop / area;
+    pibpc = pib / pop;
+
     
-    // Exibição dos Dados das Cartas:
+    // Exibição dos Dados das Cartas;
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
@@ -54,9 +59,11 @@ int main(){
     printf("O código da cidade é: %d\n", cod);
     printf("O nome da cidade é: %s\n", nome);
     printf("A população é de: %d milhões\n", pop);
-    printf("A área é: %d km2\n", area);
+    printf("A área é: %.3f km2\n", area);
     printf("O PIB é: %.3f\n", pib);    
     printf("A qtde de pontos tuirísticos: %d\n", turismo);
+    printf("A densidade populacional é de: %.3f\n", densidade);
+    printf("O PIB per capita é: %.3f\n", pibpc);
 
     return 0;
 }
